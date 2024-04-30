93082c57356f82194c8dad58459042adeb26fa2d
twondimageg1 <tensayw@ginzabe.com>
2024-04-30 09:27:25 -0400

    update ginzabe

---
+++ ginzabe/src/runable/CombaineStringValue.java
@@ -0,0 +1,15 @@
+package runable;
+
+public class CombaineStringValue {
+
+	public static void main(String[] args) {
+		// TODO 
+        int someNumber = 12;
+        int anothrNumber = someNumber + 7;
+        String someString;
+        String myName = "Tensay";
+        someString = "Here is" + " "+myName;
+        System.out.println(someString);
+	}
+
+}
---
+++ ginzabe/src/runable/GinzabeAdventaure.java
@@ -0,0 +1,20 @@
+package runable;
+
+public class GinzabeAdventaure {
+
+	public static void main(String[] args) {
+		// DECLATION
+		String G1;
+		String G2;
+
+		// INITIALIZED
+
+		G1 = "Tensay";
+		G2 = "Minte";
+
+		String addTwoName = G1 + " " + G2;
+
+		System.out.println(addTwoName);
+	}
+
+}
