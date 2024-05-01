e6860637990c426d5dbc80435608230163131b51
twondimageg1 <tensayw@ginzabe.com>
2024-04-30 21:14:20 -0400

    add on ginzabe

---
+++ ginzabe/src/runable/GinzabeAdventaure2.java
@@ -0,0 +1,17 @@
+package runable;
+
+public class GinzabeAdventaure2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String myname = "Tensay";
+    int count = 0;
+    count++;
+    count += 4;
+    count -= 4;
+    count += 2;
+    System.out.println( myname +" " + count +" " + "us states");
+    
+	}
+
+}
